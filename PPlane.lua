23a38d032918a0573180f0af74179c61c62596d4
Andrea Campanella <andrea@digiconsoles.com>
2016-05-24 14:15:02 +0100

    Removed the need for the shell script

--- PPlane.lua
+++ PPlane.lua
@@ -2,13 +2,21 @@ function on_msg_receive (msg)
     if msg.out then
         return
     end
-    if (msg.text=='ping') then
-       send_msg (msg.from.print_name, 'pong', ok_cb, false)
+    
+    if (msg.text=='Ping') then
+		send_msg (msg.from.print_name, 'pong', ok_cb, false)
     end
-    if (msg.text=='photo') then
-     os.execute('/home/pi/camera/camera.sh')
-     send_photo (msg.from.print_name, '/home/pi/camera/photo.jpg', ok_cb, false)
-     end
+    
+    if (msg.text=='Photo') then
+     	os.execute('/usr/bin/raspistill -w 800 -h 600 -o /tmp/photo.jpg')
+     	send_photo (msg.from.print_name, '/tmp/photo.jpg', ok_cb, false)
+    end
+
+    if (msg.text=='Reboot') then
+     	os.execute('/sbin/reboot')
+     	send_msg (msg.from.print_name, 'Rebooting...', ok_cb, false)
+    end
+
 end
  
 function on_our_id (id)
