a1e5fde5d58236b96e80e571585776d01635ffcf
Andrea Campanella <andrea@digiconsoles.com>
2016-05-24 15:08:24 +0100

    Added the possibility to start record video and stop it , ask for disk space

--- PPlane.lua
+++ PPlane.lua
@@ -1,10 +1,14 @@
+
 function on_msg_receive (msg)
     if msg.out then
         return
     end
+    if (msg.text=='Help') then
+        send_msg (msg.from.print_name, 'Ping,Photo,Reboot,Vrec,Vstop,Disk', ok_cb, false)
+    end
     
     if (msg.text=='Ping') then
-		send_msg (msg.from.print_name, 'pong', ok_cb, false)
+		send_msg (msg.from.print_name, 'Pong', ok_cb, false)
     end
     
     if (msg.text=='Photo') then
@@ -12,6 +16,20 @@ function on_msg_receive (msg)
      	send_photo (msg.from.print_name, '/tmp/photo.jpg', ok_cb, false)
     end
 
+    if (msg.text=='Vrec') then        
+        os.execute('/usr/bin/raspivid -o video_`date +%Y-%m-%d_%H:%M:%S`.mp4 -t 0 &')
+        send_msg (msg.from.print_name, 'Recording.', ok_cb, false)
+    end
+
+    if (msg.text=='Vstop') then        
+        os.execute('/usr/bin/killall raspivid')
+        send_msg (msg.from.print_name, 'DSone.', ok_cb, false)
+    end
+
+    if (msg.text=='Disk') then        
+        
+        send_msg (msg.from.print_name, os.execute('/bin/df -h'), ok_cb, false)
+    end
     if (msg.text=='Reboot') then
      	os.execute('/sbin/reboot')
      	send_msg (msg.from.print_name, 'Rebooting...', ok_cb, false)
